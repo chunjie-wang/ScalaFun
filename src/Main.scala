b24320f0bc771b2103f20878906f076ec2f34a8e
chunjie-wang <chunjie.wangcn@gmail.com>
2013-06-16 17:52:55 +0800

    Add a few function test cases

--- src/Main.scala
+++ src/Main.scala
@@ -1,5 +1,46 @@
 object Main extends App {
     override def main(args: Array[String]) {
       println("Hello World");
+      testFunctions();
+    }
+    
+    /**
+     * Functions test cases
+     */
+    def testFunctions() {
+        /**
+         * Anonymous functions 
+         */
+        val addOne = (x: Int) => x + 1
+        println("addOne result %d".format(addOne(3)));
+        
+        /**
+         * With return type specified
+         */
+        def timesTwo(i: Int): Int = {
+           println("times Two");
+           i*2;
+        }
+        println("timesTwo result %d".format(timesTwo(3)));
+        
+        /**
+         * Return type is inferred by complier automatically.
+         */
+        def sum(m: Int, n: Int) = m+n
+        println("sum result %d".format(sum(3,4)));
+        
+        /**
+         * Partial application
+         */
+        val sum10 = sum(10, _:Int)
+        println("sum result %d".format(sum10(4)));
+        
+        def capitalizeAll(args: String*) = {
+            args.map { arg =>
+                arg.capitalize
+            }
+        }
+        
+        capitalizeAll("abc", "cde");
     }
 }
