cd29b1280b98a4690e466bf8950b6f69a22c5717
chunjie-wang <chunjie.wangcn@gmail.com>
2013-06-17 16:41:54 +0800

    Study in progress

--- src/Main.scala
+++ src/Main.scala
@@ -2,8 +2,11 @@ package src
 
 object TestApp {
   def main(args: Array[String]) {
-    //testFunctions();
+    testFunctions();
     testClass();
+    testLoop();
+    testClosures();
+    testHigherOrder();
   }
 
   /**
@@ -63,6 +66,17 @@ object TestApp {
     
     def sub(a: Int, b: Int): Int = a - b;
     
+    def whichDay(month: Int, leap: Boolean) = {
+      month match {
+      case 2 if leap        => 29
+      case 2                => 28
+      case 4 | 6 | 9 | 11   =>  30
+      case _                => 31
+      }
+      
+    }
+    
+    printf("Which day it is: %d \n", whichDay(2, false))
   }
 
   def testClass() {
@@ -84,5 +98,79 @@ object TestApp {
     val x = new B(1, 0, 2);
     println("Values: %d %d".format(x.a, x.b))
     println("call function foo: %d ".format(x.foo(3)))
+  }
+  
+  /**
+   * Test loop: While, For etc.
+   */
+  def testLoop() {
+    var a = 0
+    var b = 0
+    // for
+    for (a <- 1 to 3; b <- a to 3) {
+      println("a & b: " + a +" " + b)
+    }
+    
+    val l = List(1, 2, 5, 7)
+    // for with filters
+    for(x <- l if x < 5; if x % 2 == 0) 
+      println("x in list: " + x);
+    
+    // get a list of number via yield
+    var ret = for {x <- l if x < 7; if x % 2 == 0} yield x
+    println(ret)
+    
+    // while
+    while (a < 3) {
+      println("a = " + a);
+      a += 1
+    }
+    
+    // Array
+    val arr = new Array[Int](5)
+    for (i <- 0 to (arr.length - 1))
+      arr(i) = i
+    for (x <- arr) println("x = " + x + " ");
+    
+    val twoD = Array.ofDim[Int](2,3)
+    
+    
+  }
+  
+  /**
+   * Test closure in Scala
+   */
+  def testClosures() {
+    val factor = 10
+    val fac = (i: Int) => i * factor
+    println("factor as 10 " + fac(10))
+  }
+  
+  /**
+   * Test higher-order functions
+   */
+  def testHigherOrder() {
+    trait x[a] { 
+      def map[b](func: a=>b):x[b]
+    }
+    
+    val ans = (1 to 10) map (x => x * 2)
+    val ans2 = (1 to 10) map (_*2)
+    println(ans)
+    
+    def apply(f: Int => String, v: Int) = f(v)
+    def layout[A](x: A) = "[" + x.toString() + "]"
+    println(apply(layout, 10))
+    
+    // Define filter and take predicate
+    val even = (x: Int) => x % 2 == 0
+    val odd = (x: Int) => x % 2 == 1
+    def filter(predicate: Int=>Boolean, arr: List[Int]) = {
+      for (x <- arr; if predicate(x)) yield x
+    }
+   
+    val arr = List[Int](1, 3, 4)
+    val res = filter(even, arr)
+    println(res)
   }
 }
